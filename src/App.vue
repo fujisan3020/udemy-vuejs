f86698b4180ae055c5f23ae2121a39026b60104c
fujisan <gashigashi3040@gmail.com>
2020-05-27 17:09:19 +0900

    #112. 動的に複数のコンポーネントを切り替えるために、componentタグと、is属性を使用する

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,36 @@
 
 <template>
   <div>
-    <LikeHeader #default="slotProps">
-        <p>{{ slotProps }}</p>
-        <h2>みなさん</h2>
+    <LikeHeader>
         <h3>はじめまして</h3>
-        <p>よろしくお願いします</p>
-
-        <!-- v-slot: は # に置き換えることができる -->
-
-        <!-- 動的なスロット -->
-        <!-- <template v-slot:[title]></template> -->
-        <!-- <template #[title]></template> -->
     </LikeHeader>
 
-
-
     <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :total-number="number"></LikeNumber>
+
+    <button @click="currentComponent = 'Home' ">Home</button>
+    <button @click="currentComponent = 'About' ">About</button>
+    <!-- 動的コンポーネント : componentタグとis属性を使用して作成する -->
+    <component :is="currentComponent"></component>
   </div>
 </template>
 
 <script>
 // ローカル登録
 import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
   export default {
     data() {
       return {
         number: 14,
-        title: "title"
-      }
+        currentComponent: "Home"
+      };
     },
     components: {
-      LikeHeader
+      LikeHeader,
+      About,
+      Home
     },
     methods: {
       incrementNumber(value) {
--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -2,12 +2,8 @@
   <div>
     <!-- slot : 親コンポーネントから子コンポーネントにHTMLタグを含んだデータを渡すことができる -->
 
-    <slot :user="user" text="text"></slot>
     <slot></slot>
-    <hr>
-    <p>いいねの数</p>
-    <slot></slot>
-
+    
   </div>
 </template>
 
