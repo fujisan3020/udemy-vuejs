b7e1c9433050497c6167bef322c9338e2a642416
fujisan <gashigashi3040@gmail.com>
2020-05-27 17:19:18 +0900

    #113. 動的コンポーネントは切り替えるごとにdestroyedされる挙動となることを理解する

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,15 @@
 <template>
-  <p>About</p>
+  <div>
+    <p>About</p>
+      <input>
+  </div>
 </template>
+
+<script>
+// 動的コンポーネントは切り替えるごとにコンポーネントがdestroyされ、新しいインスタンスが再生成されるようになっている
+export default {
+  destroyed() {
+    console.log("destroyed");
+  }
+}
+</script>
