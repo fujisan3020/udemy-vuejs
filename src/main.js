3cc76eb81c1fd33739967e0ad32c1599aad5c7d7
fujisan <gashigashi3040@gmail.com>
2020-05-29 10:25:32 +0900

    131~133 カスタムディレクティブ グローバル登録とフック関数

--- src/main.js
+++ src/main.js
@@ -7,7 +7,31 @@ import LikeNumber from "./components/LikeNumber.vue";
 Vue.config.productionTip = false;
 // グローバル登録
 // main.jsでimportして来て、Vue.componentを宣言する
+
+// カスタムディレクティブは抽象化したコードを再利用したいときに使う
 Vue.component('LikeNumber', LikeNumber);
+Vue.directive("border", {
+  // v-bind : ディレクティブが対象の要素に紐付いた時
+  bind(el, binding, vnode) {
+
+  },
+  // inserted : 親Nodeが挿入された時
+  inserted(el, binding, vnode) {
+
+  },
+  // update : コンポーネントが挿入される度。子コンポーネントが更新される前。
+  update(el, binding, vnode, oldVnode) {
+
+  },
+  // componentUpdataed : コンポーネントが挿入される度。子コンポーネントが更新される後。
+  componentUpdataed(el, binding, vnode, oldVnode) {
+
+  },
+  // unbind : ディレクティブが紐付いていた要素から取り除かれた時
+  unbind(el, binding, vnode) {
+
+  }
+});
 
 new Vue({
   // E6のrender関数の記載方法
