332f1a07c1af2de0d7d401105a78339996884a94
fujisan <gashigashi3040@gmail.com>
2020-05-29 11:16:35 +0900

    #140. directivesオプションを使って、ローカルにカスタムディレクティブを登録する

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,3 +4,25 @@
   <!-- 引数の後に、修飾子をいくつか設定できる -->
   <p v-border:solid.round.shadow=" {width: '5px', color: 'red'} ">Home</p>
 </template>
+
+<script>
+export default {
+  // directivesオプションを使えば、カスタムディレクティブをローカル登録できる
+  directives: {
+    border(el, binding) {
+      // binding.valueでデータを受け取ることができる
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      // binding.argで引数の値を取る
+      el.style.borderStyle = binding.arg;
+      // 修飾子の設定
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.5)";
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -9,21 +9,6 @@ Vue.config.productionTip = false;
 // main.jsでimportして来て、Vue.componentを宣言する
 
 Vue.component('LikeNumber', LikeNumber);
-// カスタムディレクティブは抽象化したコードを再利用したいときに使う
-Vue.directive("border", function(el,binding) {
-  // binding.valueでデータを受け取ることができる
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  // binding.argで引数の値を取る
-  el.style.borderStyle = binding.arg;
-  // 修飾子の設定
-  if (binding.modifiers.round) {
-    el.style.borderRadius = "0.5rem";
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.5)";
-  }
-});
 
 new Vue({
   // E6のrender関数の記載方法
