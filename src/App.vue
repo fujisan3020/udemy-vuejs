9a7d54cb0f90bc8038c4b502568f3ed93be2e7ac
fujisan <gashigashi3040@gmail.com>
2020-06-01 05:06:08 +0900

    #174. 実際にJavaScriptフックを使用してアニメーションを作成する

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,12 @@
     <button @click="show = !show">切り替え</button>
     <br><br>
     <!-- JavaScriptフック -->
-    <!-- css属性 : falseを渡すことによって、cssのアニメーションは適応させない ということを明示することができる。cssがトランジションに干渉するのを無効化させたり、cssの判定をスキップさせることで、パフォーマンスを向上させることができる。
-    <transition -->
+    <!-- css属性 : falseを渡すことによって、cssのアニメーションは適応させない ということを明示することができる。cssがトランジションに干渉するのを無効化させたり、cssの判定をスキップさせることで、パフォーマンスを向上させることができる。 -->
+    <transition
       :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @entwr-cancelled="enterCancelled"
-
-      @before-leave="beforeLeave"
       @leave="leave"
-      @after-leave="afterLeave"
-      @leave-cancelled="leaveCancelled"
     >
       <div class="circle" v-if="show"></div>
     </transition>
@@ -73,28 +67,31 @@ export default {
   methods: {
     beforeEnter(el) {
       // 現れる前
+      el.style.transform = `scale(0)`
     },
     enter(el, done) {
       // 現れる時
-    },
-    afterEnter(el) {
-      // 現れた後
-    },
-    enterCancelled(el) {
-      // 現れるアニメーションがキャンセルされれた時
-    },
-    beforeLeave(el) {
-      // 消える前
+      let scale = 0;
+      const interval = setInterval(() =>{
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1;
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
     leave(el, done) {
       // 消えた時
-    },
-    afterLeave(el) {
-      // 消えた後
-    },
-    leaveCancelled(el) {
-      // 消えるアニメーションがキャンセルされれた時
-      // v-showと一緒に使う時だけ、有効になる
+      let scale = 1;
+      const interval = setInterval(() =>{
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1;
+        if (scale < 0) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     }
   }
 }
