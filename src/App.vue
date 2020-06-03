f9853702fcba861b8131582919e5ab9363e9bdd9
fujisan <gashigashi3040@gmail.com>
2020-06-04 04:26:44 +0900

    #186. active-class属性とexact属性を使って、アクティブなリンクにスタイリングをつける

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,21 @@
       <!-- <a>ではなく、<router-link>コンポーネントを使って、クリックでURLを切り替える -->
       <!-- <router-link>ではhref=""の代わりに、to=""を使う -->
       <!-- また、tagを設定することで、tagを変更できる(<a>としてのおおもとの機能は変わらない?)。 -->
-      <router-link to="/" tag="div" class="link">Home</router-link>
+      <!-- active-class属性 : アクティブなリンクにスタイリングをつける。 -->
+      <!-- exact属性 : リンクに対して "正確なマッチモード" を強制する -->
+      <router-link
+        to="/" tag="div"
+        active-class="link-active"
+        exact
+        class="link"
+      >Home</router-link>
       <br>
-      <router-link to="/users" class="link">Users</router-link>
+      <router-link
+        to="/users"
+        active-class="link-active"
+        exact
+        class="link"
+      >Users</router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -15,5 +27,8 @@
 <style scoped>
   .link {
     margin-right: 10px;
+  }
+  .link-active {
+    font-size: 20px;
   }
 </style>
