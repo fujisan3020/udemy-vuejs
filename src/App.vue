363b4a63297e971806c91c60be6c192f8d222946
fujisan <gashigashi3040@gmail.com>
2020-06-03 18:08:35 +0900

    #184. <router-link>コンポーネントを使って、クリックでURLを切り替える

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <!-- <a>ではなく、<router-link>コンポーネントを使って、クリックでURLを切り替える -->
+      <!-- <router-link>ではhref=""の代わりに、to=""を使う -->
+      <!-- また、tagを設定することで、tagを変更できる(<a>としてのおおもとの機能は変わらない?)。 -->
+      <router-link to="/" tag="div" class="link">Home</router-link>
+      <br>
+      <router-link to="/users" class="link">Users</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+</style>
