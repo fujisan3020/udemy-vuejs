fe2040b774337314012fc33f757c3e529babfb04
fujisan <gashigashi3040@gmail.com>
2020-06-04 04:54:54 +0900

    #190. ルートコンポーネントの再利用性を損なわないようにするpropsオプションをtrueにする

--- src/router.js
+++ src/router.js
@@ -12,7 +12,8 @@ export default new Router({
   mode: "history",
   routes: [
     {path: "/", component: Home},
-    {path: "/users/:id", component: Users}
+    // ルートコンポーネントの再利用は、propsオプションをtrueにする
+    {path: "/users/:id", component: Users, props: true}
   ]
 })
 
